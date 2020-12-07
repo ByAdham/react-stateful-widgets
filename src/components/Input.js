0a280a3078c3886c0f00273808876cc88daf912b
Adham AbdelFattah <adham.adel@gmail.com>
2020-12-07 16:35:35 -0600

    mostly done

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,5 +1,5 @@
 /*
-INPUT Instructions
+INPUT Instructions TEST
 
 Watch this short video:
 https://tk-assets.lambdaschool.com/a664dfe7-2dde-48d1-8fa2-48066d98e515_input.gif
@@ -34,34 +34,39 @@ STEP 6:
   We need to add an extra prop to the <input /> element like so: value={inputValue}
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Input() {
   /* STEP 1 */
-
+  const [inputValue, setInputValue] = useState("");
   const changeInput = evt => {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
-    const { value } = evt.target;
+    // (evt.target);
+    // console.log(evt.target.value);
+    const {value} = evt.target
+    setInputValue(value);
+    
 
     /* STEP 4 */
   };
   const reset = () => {
     /* STEP 5 */
+    setInputValue("");
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: inputValue.length > 10 ? 'red' : 'royalblue', /* STEP 2 */
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}></div> {/* STEP 3 */}
+  <div id='output' style={style}> {inputValue.toUpperCase()} </div> {/* STEP 3 */}
       <div>
-        <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
+        <input type='text' onChange={changeInput} /> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
       </div>
     </div>
